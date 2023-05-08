7155e95511eab8e0124c6ace451a50b88a747b0d
Adrien Fichet <adrien.fichet@gmail.com>
2023-05-08 14:20:15 +0200

    Add rsync_to_remote.py

---
+++ rsync_to_remote.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+from subprocess import call
+from pathlib import Path
+from vars import SSH_USER, SSH_PASSWORD_FILE, SSH_HOST
+
+
+def main():
+    src = Path.home() / "phone_backup"
+    dst = f"/home/{SSH_USER}/phone_backup"
+    password = SSH_PASSWORD_FILE.read_text()
+    call(f"sshpass -p {password} /usr/bin/rsync -avz --progress {src}/ {SSH_USER}@{SSH_HOST}:{dst}/", shell=True)
+
+
+if __name__ == "__main__":
+    main()
